64c4cefc778a9656d028ab077b59f24ebf053be3
Oswaldo Granados <ogranados@ts4.mx>
2021-11-05 20:42:58 -0600

    commit agregando node

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,10 @@
+// export var GLOBAL = {
+//     url: 'http://localhost:3977/api/',
+//     urlComponents: 'http://localhost:4200/'
+// }
+
+//Heroku
 export var GLOBAL = {
-    url: 'http://localhost:3977/api/',
-    urlComponents: 'http://localhost:4200/'
+    url: 'https://api-orthosmile.herokuapp.com/api/',
+    urlComponents: 'https://client-orthosmile.herokuapp.com/'
 }
